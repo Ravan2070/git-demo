4d09bf83a920cc14f4781ef491717bd37103ccc4
Ravan2070 <crahate11@gmail.com>
2021-06-16 15:50:29 +0530

    Square function implementation on Feature1 branch

--- math.py
+++ math.py
@@ -11,7 +11,12 @@ def multiply(x,y):
     return x*y		   # on Bug456 branch
 
 # Divide implementation
+def divide(x,y):
     if y==0:             
 	return DIVIDE_BY_0_ERROR
     else:
 	return x/y
+
+# Square implementation
+def square(x):
+	return x*x
