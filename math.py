9b831c3ed66834d937f4525e39cd0c5b77f0bf5d
Ravan2070 <crahate11@gmail.com>
2021-06-16 15:42:17 +0530

    Divide function implementation on Bug789 branch

--- math.py
+++ math.py
@@ -11,5 +11,7 @@ def multiply(x,y):
     return x*y		   # on Bug456 branch
 
 # Divide implementation
-def divide(x,y):
-    return x/y		#on main branch
+    if y==0:             
+	return DIVIDE_BY_0_ERROR
+    else:
+	return x/y
